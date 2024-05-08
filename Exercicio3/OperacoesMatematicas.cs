ee0d48d09066985ac02ad9c9b246930cf4f0b1da
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-05-08 17:02:22 -0300

    Adicionado Exercicio 3

---
+++ Exercicio3/OperacoesMatematicas.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace Exercicio3
+{
+    public class OperacoesMatematicas
+    {
+        public double Somar(double numero1, double numero2)
+        {
+            var result = numero1 + numero2;
+
+            return(result);
+        }
+
+        public double Subtrair(double numero1, double numero2)
+        {
+            var result = (numero1 - numero2);
+
+            return(result);
+        }
+
+        public double Multiplicar(double numero1, double numero2)
+        {
+            var result = (numero1 * numero2);
+
+            return(result);
+        }
+
+        public object Dividir(double numero1, double numero2)
+        {
+            var result = new object();
+
+            if(numero1 == 0 || numero2 == 0)
+            {
+                result = "Todo numero dividido por 0 é 0";
+                return result;
+            }
+            else { 
+                result = numero1 / numero2;
+                return result;
+            }
+        }
+
+        public double Media(double numero1, double numero2)
+        {
+            var result = (numero1 + numero2) / 2.00;
+
+            return(result);
+        }
+
+
+    }
+}
---
+++ Exercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio3;
+
+class Program
+{
+    static void Main()
+    {
+        double numero1 = 25.00;
+        double numero2 = 50.00;
+
+        var operacoesMatematicas = new OperacoesMatematicas(); 
+
+        Console.WriteLine(operacoesMatematicas.Somar(numero1: numero1, numero2: numero2));
+        Console.WriteLine(operacoesMatematicas.Subtrair(numero1: numero1, numero2: numero2));
+        Console.WriteLine(operacoesMatematicas.Dividir(numero1: numero1, numero2: numero2));
+        Console.WriteLine(operacoesMatematicas.Multiplicar(numero1: numero1, numero2: numero2));
+        Console.WriteLine(operacoesMatematicas.Media(numero1: numero1, numero2: numero2));
+    }
+}
