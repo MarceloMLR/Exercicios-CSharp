39b7d05852d3f32916879a2326d6c0cfaa3fb793
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-05-08 16:40:00 -0300

    Add project files.

---
+++ Exercicio-1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio1;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá, digite o seu nome");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine($"Olá {nome}, seja bem vindo!");
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio2;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o seu nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Digite o seu sobrenome:");
+        string sobrenome = Console.ReadLine();
+
+        string nomeCompleto = string.Join(" ", nome, sobrenome);
+
+        Console.WriteLine($"Olá! {nomeCompleto}");
+    }
+}
